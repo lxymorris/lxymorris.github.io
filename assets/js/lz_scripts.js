7480d8519cddcac83295c64ea6ee79c488b6ad59
lxymorris <lxy.morris@gmail.com>
2024-05-15 21:41:03 -0700

    Initial commit

---
+++ assets/js/lz_scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#footer-container").load("components/footer.html");
+});
+
+$(document).ready(function(){
+    $("#header-container").load("components/header.html");
+});
+
+// $(document).ready(function() {
+//     $("#header-container").load("components/header.html", function() {
+//         // Reinitialize dropdowns or other plugins
+//         $('.submenu').dropotron({
+// 			mode: 'fade',
+// 			noOpenerFade: true,
+// 			expandMode: (browser.mobile ? 'click' : 'hover')
+// 		});
+        
+//     });
+// });

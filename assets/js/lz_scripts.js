82edfb3883fc484d2c100665036e802ed265f803
lukasOnLexyBehalf <lxy.morris@gmail.com>
2024-05-17 16:14:22 -0700

    more changes

--- assets/js/lz_scripts.js
+++ assets/js/lz_scripts.js
@@ -6,14 +6,21 @@ $(document).ready(function(){
     $("#header-container").load("components/header.html");
 });
 
-// $(document).ready(function() {
-//     $("#header-container").load("components/header.html", function() {
-//         // Reinitialize dropdowns or other plugins
-//         $('.submenu').dropotron({
-// 			mode: 'fade',
-// 			noOpenerFade: true,
-// 			expandMode: (browser.mobile ? 'click' : 'hover')
-// 		});
-        
-//     });
-// });
+// // Function to load content dynamically and update URL without file extension
+// function loadContent() {
+//     const hash = window.location.hash.substr(1);
+//     const page = hash || 'index'; // Default to 'home' if no hash is present
+
+//     fetch(page + '.html')
+//         .then(response => response.text())
+//         .then(html => {
+//             document.getElementById('content').innerHTML = html;
+//         })
+//         .catch(error => console.error('Error loading the page: ', error));
+// }
+
+// // Event listener to handle hash changes
+// window.addEventListener('hashchange', loadContent);
+
+// // Initial load of content based on the hash
+// loadContent();
